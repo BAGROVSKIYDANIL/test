82b0cc35960bcca58afe3e502d045069fcaa3700
BAGROVSKIY <mrkenay@mail.ru>
2022-10-24 14:36:47 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,15 @@
+"use sctrict";
+let incr = 10,
+    decr = 10;
+
+// incr++;
+// decr--;
+
+console.log(incr++);
+console.log(decr--);
+console.log(2*4 === 8);
+
+const isChecked = true,
+      isclose = false;
+
+console.log(isChecked && isclose);
